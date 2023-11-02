4afbc505e72220591c3043641747d6e385033d0c
Deligent2 <148380705+Deligent2@users.noreply.github.com>
2023-11-02 21:19:10 +0300

    finished

---
+++ script.js
@@ -0,0 +1,25 @@
+// const arr = [1, 2, 3, 4, 5];
+// let result = 0;
+// for (let i = 0; i < arr.length; i++) {
+//     result += arr[i];
+// }
+// console.log(result);
+
+// const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// const result = arr.reduce((acc, el) => acc + el, 0)
+// console.log(result); 
+
+
+// const arr = [1, 2, 5, 9, 4, 13, 4, 10];
+// let found = false;
+
+// for (let i = 0; i < array.length; i++) {
+//     if (arr[i] === 4) {
+//         found = true;
+//         console.log('Есть!');
+//         break;
+//     }
+// }
+// if (!found) {
+//     console.log('Нет!');
+// }
